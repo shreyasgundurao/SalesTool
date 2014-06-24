ce7054ba9ef7b223bda7b29fae8871e294ddd040
gundurao <gundurao@GUNDURAO2.americas.hpqcorp.net>
2014-06-23 17:12:19 -0700

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author gundurao
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData salesData = new SalesData();
+		salesData.display();
+		
+		System.out.println("Exiting now. Thanks!");
+
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author gundurao
+ *
+ */
+public class SalesData {
+	
+	public void display() {
+		System.out.println("Displaying sales data in SalesData Class");
+	}
+
+}
